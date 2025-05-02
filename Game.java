9b36269cddb542113c078e461d6cd98f44b6053d
mfedorchenko <mfedorchenko@ethz.ch>
2025-05-02 11:05:37 +0200

    feat: player can find an object and bring it to the church for a happy ending :)

--- Game.java
+++ Game.java
@@ -3,16 +3,21 @@ import java.util.Scanner;
 public class Game {
     private Room currentRoom;
     private Room wald;
+    private Room friedhof;
+    private Room kirche;
+    private Room schloss;
+    private Room taverne;
+    boolean objectTaken = false;
 
     public Game() {
         createRooms();
     }
 
     private void createRooms() {
-        Room schloss = new Room("Du stehst vor dem alten Schloss.");
-        Room taverne = new Room("Du bist in der Taverne. Es riecht nach Bier und Rauch.");
-        Room kirche = new Room("Die Kirche ist kalt und leer.");
-        Room friedhof = new Room("Nebelschwaden ziehen über den Friedhof.");
+        schloss = new Room("Du stehst vor dem alten Schloss.");
+        taverne = new Room("Du bist in der Taverne. Es riecht nach Bier und Rauch.");
+        kirche = new Room("Die Kirche ist kalt und leer.");
+        friedhof = new Room("Nebelschwaden ziehen über den Friedhof.");
         wald = new Room("Du stehst am Rand eines dunklen Waldes... Niemand kehrt von hier zurück.");
 
 
@@ -26,7 +31,7 @@ public class Game {
         currentRoom = schloss;
     }
 
-    private void goRoom(String direction) {
+    private void goRoom(String direction, Scanner scanner) {
         Room nextRoom = null;
     
         switch (direction) {
@@ -49,7 +54,24 @@ public class Game {
                 System.out.println("\nEin kalter Schauer läuft dir über den Rücken...");
                 System.out.println("Du hast den Wald betreten und wirst nie wieder gesehen.\nGAME OVER");
                 System.exit(0);
+            }else if (currentRoom == friedhof && objectTaken == false) {
+                System.out.println("\nDu laeufst um das Friedhof herum. Du spürst die tote Seelen schauen dich zu von einen Gräben");
+                System.out.println("\nUnd doch findest du ein uralter halbstreckender aus einem alten Graben Objekt, verstehst aber nicht was es genau ist. Mitnehmen? (ja/nein)");
+                System.out.print("> ");
+                String antwort = scanner.nextLine().trim().toLowerCase();
+                
+                if (antwort.equals("ja")) {
+                    System.out.println("Du nimmst es mit. Was machst du jetzt?");
+                    objectTaken = true;
+                }else {
+                    System.out.println("Du lässt das Objekt liegen. Was ist, wenn es verflucht ist?");
+                }
+            } else if (currentRoom == kirche && objectTaken == true) {
+                System.out.println("Du bist wieder in der Kirche. Jedoch felt es dir erst jetzt auf, dass in dem weitesten, dünkleren Ecke des Raums steht es ein Opferaltar. \nDu entscheidest, den Objekt von Friedhof dadrauf zu stellen. Und - du siehst den hellen zum Himmel ausstralenden Licht. \nEs ist ein guter Zeichen. \nTO BE CONTINUED");
+                System.exit(0);
             }
+ 
+
         }
     }
     
@@ -71,7 +93,7 @@ public class Game {
                 System.out.println(currentRoom.getDescription());
             } else if (input.startsWith("go ")) {
                 String direction = input.substring(3);
-                goRoom(direction);
+                goRoom(direction, scanner);
             } else {
                 System.out.println("Ich kenne diesen Befehl nicht.");
             }
